64913fab05983c711d7de0e0b9947e53defd7bda
YanGao19 <56466955+YanGao19@users.noreply.github.com>
2020-09-25 17:46:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// 3. 通过 ReactDOM 将 App.js 以虚拟 DOM 的形式渲染/挂载到 root 根节点，该节点在 index.html 中
+ReactDOM.render(		
+	<App />, 
+	document.getElementById('root')
+	);
